68016aa1fd2b05c8e59ba8c7a3f5afd695b1c656
H <hruiz17@hotmail.com>
2024-12-10 06:36:44 -0500

    Proyecto base

---
+++ src/main/java/com/tutorial/tutorial_webflux/TutorialWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.tutorial_webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialWebfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutorial/tutorial_webflux/TutorialWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.tutorial_webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
